1ce483f12d11c4b4f8cf5b9c85f1ea35daf4daa9
rajawiyat <144016251+rajawiyat@users.noreply.github.com>
2023-09-04 14:11:46 +0100

    Add files via upload

---
+++ ZZ.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+ 
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Modern PHP Page</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <header>
+        <h1>Bienvenue sur ma page élégante</h1>
+    </header>
+    <nav>
+        <ul>
+        
+        <img src=" 7.png" alt="Description of the image" width="60" height="80">
+            <li><a href="#">Home</a></li>
+            <li><a href="#">About</a></li>
+            <li><a href="#">Services</a></li>
+            <li><a href="#">Contact</a></li>
+        </ul>
+    </nav>
+    <main> 
+    <h1>QR CODE TO EXCEL</h1>
+    <video width="640" height="360" controls>
+        <source src=" RecForth/20230823003014.mp4" type="video/mp4">
+        Your browser does not support the video tag.
+    </video>
+   
+
+<h1>QR CODE generated</h1>
+    <video width="640" height="360" controls>
+        <source src="RecForth/20230826060527.mp4" type="video/mp4">
+        Your browser does not support the video tag.
+    </video>
+
+<h1>APLICATION generation MAIN DEUVRE</h1>
+    <video width="640" height="360" controls>
+        <source src="RecForth/20230826061134.mp4 " type="video/mp4">
+        Your browser does not support the video tag.
+    </video>
+
+<h1>QR CODE</h1>
+    <video width="640" height="360" controls>
+        <source src="RecForth/20230827055800.mp4" type="video/mp4">
+        Your browser does not support the video tag.
+    </video>
+
+<h1>QR CODE</h1>
+    <video width="640" height="360" controls>
+        <source src="RecForth/20230902233359.mp4" type="video/mp4">
+        Your browser does not support the video tag.
+    </video>
+
+   
+   
+   
+   
+   
+   
+   <?php
+        // Your PHP logic and dynamic content generation here
+        echo "<p>CONTACTER PORE MOI 212690919779.</p>";
+        ?>
+    </main>
+    <footer>
+    <h1>Sample Form</h1>
+    <form action="process_form.php" method="post">
+        <label for="name">Name:</label>
+        <input type="text" id="name" name="name" required><br><br>
+
+        <label for="email">Email:</label>
+        <input type="email" id="email" name="email" required><br><br>
+
+    
+        <input type="submit" value="Submit">
+    </form>
+        <p>&copy; <?php echo date("Y"); ?> My Website</p>
+    </footer>
+    
+</body>
+</html>
